3392eb284cbf5643e723ef478635f98fcc830c15
mugdan0000 <esther.mugdan@unibas.ch>
2022-10-14 11:12:26 +0200

    fixed password

--- resources/password.js
+++ resources/password.js
@@ -2,7 +2,8 @@ var pwi;
 var pw = "rk-yc-22";
 pwi = prompt("Bitte geben Sie das Passwort ein um diesen Inhalt abzurufen.", "");
 while (pwi != pw) {
-    alert("Falsches Passwort!")
-    pwi = prompt("Bitte geben Sie das Passwort ein um diesen Inhalt abzurufen.", "");
-    //window.location = "../teaching.html"
+    if (pwi == null) {
+        window.location = "index.html"
+    }
+    pwi = prompt("Falsches Passwort\nBitte geben Sie das Passwort ein um diesen Inhalt abzurufen.", "");
 }
